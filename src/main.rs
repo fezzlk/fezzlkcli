7a3ec0de37af0bb62d689a75968283586056094f
fezzlk <fezzlk796@gmail.com>
2021-09-10 00:16:02 +0900

    apply tokio

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,23 @@ struct Args {
     target: String,
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     // 引数の取得
     let args = Args::from_args();
     let url: &str = "https://github.com/fezzlk";
     let api_token: &str = dotenv!("WEBEV_API_TOKEN");
     let server_url: &str = dotenv!("WEBEV_SERVER_URL");
+    println!("api_token: {}", api_token);
 
     let params = [("url", url), ("apiTokenForExtension", api_token)];
-    let client = reqwest::Client::new();
-    let res = client.post(server_url)
+    let client = Client::new();
+    let response = client.post(server_url)
         .form(&params)
-        .send();
+        .send()
+        .await?;
 
+    println!("response: {:?}", response);
     println!("command: {}", &args.command);
     println!("target: {}", &args.target);
     Ok(())
