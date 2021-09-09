4826c3954c5bd3ee3b32c1a217159cd3105fea89
fezzlk <fezzlk796@gmail.com>
2021-09-09 22:55:34 +0900

    try to send get request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use structopt::StructOpt;
 use anyhow::{Context, Result};
+use std::io::{stdout, Write};
+use curl::easy::Easy;
 
 #[derive(StructOpt)]
 struct Args {
@@ -10,6 +12,17 @@ struct Args {
 fn main() -> Result<()> {
     // 引数の取得
     let args = Args::from_args();
+
+    let mut easy = Easy::new();
+    
+    easy.url("https://www.rust-lang.org/").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    println!("{}", easy.response_code().unwrap());
 
     println!("command: {}", &args.command);
     println!("target: {}", &args.target);
