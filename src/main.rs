707d5d88c91e273e27ceb58e0183c6f7f8ab9a8b
fezzlk <fezzlk796@gmail.com>
2021-09-09 23:56:28 +0900

    read dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate dotenv;
+#[macro_use]
+extern crate dotenv_codegen;
+
+use dotenv::dotenv;
 use structopt::StructOpt;
 use anyhow::{Context, Result};
 use reqwest::Client;
@@ -11,19 +16,16 @@ struct Args {
 fn main() -> Result<()> {
     // 引数の取得
     let args = Args::from_args();
+    let url: &str = "https://github.com/fezzlk";
+    let api_token: &str = dotenv!("WEBEV_API_TOKEN");
+    let server_url: &str = dotenv!("WEBEV_SERVER_URL");
 
-    const url: &str = "";
-    const api_token: &str = "";
-    const server_url: &str = "";
-
-    // This will POST a body of `foo=bar&baz=quux`
     let params = [("url", url), ("apiTokenForExtension", api_token)];
     let client = reqwest::Client::new();
     let res = client.post(server_url)
         .form(&params)
         .send();
 
-    // apiTokenForExtension
     println!("command: {}", &args.command);
     println!("target: {}", &args.target);
     Ok(())
